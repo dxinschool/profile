141c84e3c7d0d46346cb6287c9062843fd5c7a0a
dxinschool <124847139+dxinschool@users.noreply.github.com>
2024-02-28 21:16:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+// Can't inspect element, contextmenu
+document.addEventListener("contextmenu", (e) => e.preventDefault());
+
+function ctrlShiftKey(e, keyCode) {
+	return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+
+// Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+document.onkeydown = (e) => {
+	if (
+		event.keyCode === 123 ||
+		ctrlShiftKey(e, "I") ||
+		ctrlShiftKey(e, "J") ||
+		ctrlShiftKey(e, "C") ||
+		(e.ctrlKey && e.keyCode === "U".charCodeAt(0))
+	) {
+		return false;
+	}
+};
+document.addEventListener("visibilitychange", function() {
+      var pageTitle = document.getElementById("pageTitle");
+
+      if (document.hidden) {
+        // The user is not currently viewing the page
+        pageTitle.textContent = "Please come back :((";
+      } else {
+        // The user is viewing the page
+        pageTitle.textContent = "DXuwu's site :DD";
+      }
+    });
+
 document.addEventListener("DOMContentLoaded", function() {
   const homeLink = document.getElementById("home-link");
   const aboutLink = document.getElementById("about-link");
